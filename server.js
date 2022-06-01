a1ad73003761d6d613f23fdaf89fe0b6775a0d00
Darshan Pandya <darshan.pandya@biztechcs.com>
2022-06-01 10:10:41 +0530

    Feat: Added api script to run api

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = process.env.APP_PORT
-const mongourl = process.env.MONGO_URL
 const { connectAppAPIMongoDB } = require('./services/mongodb.service');
 require('dotenv').config();
+const port = process.env.APP_PORT
+const mongourl = process.env.MONGO_URL
 
-connectAppAPIMongoDB().then((res) => {
+connectAppAPIMongoDB(mongourl).then((res) => {
 
     app.get('/', (req, res) => {
         res.send('Server is up !!')
