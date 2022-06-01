07e7007e430eee336a2539a16e78765e9ba69c5d
Darshan Pandya <darshan.pandya@biztechcs.com>
2022-06-01 10:06:34 +0530

    Feat: Added endpoints in server

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-require('dotenv').config();
 const port = process.env.APP_PORT
 const mongourl = process.env.MONGO_URL
 const { connectAppAPIMongoDB } = require('./services/mongodb.service');
+require('dotenv').config();
 
 connectAppAPIMongoDB().then((res) => {
 
@@ -11,7 +11,25 @@ connectAppAPIMongoDB().then((res) => {
         res.send('Server is up !!')
     })
 
+    app.post('/user', (req, res) => {
+        //Add user in db generate JWT
+        res.send('Signup successfull')
+    })
 
+    app.post('/user/login', (req, res) => {
+        //Auth user and add session, res with token and logout if already logged in
+        res.send('Login successfull')
+    })
+
+    app.post('/tip/calculate', (req, res) => {
+        //Tip calculation
+        res.send('Data')
+    })
+
+    app.get('/tip', (req, res) => {
+        //Get tip data with query params
+        res.send('Data')
+    })
 
     app.listen(port, () => {
         console.log(`Example app listening on port ${port}`)
